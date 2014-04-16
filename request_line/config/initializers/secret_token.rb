cc490c187071b87e6e596e041a73b8add85d1fc7
Natalie <blackburn87@gmail.com>
2014-04-15 18:13:23 -0700

    adds user authentication

---
+++ request_line/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RequestLine::Application.config.secret_key_base = '77fe19b961237bd96e379879bdf90d97bf13e987fdde26477483a7828fbe68ac1078028387c1abf68cecd055de12344b11a616eaea9cc0f24bc769c93d48156b'
---
+++ request_line/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RequestLine::Application.config.session_store :cookie_store, key: '_request_line_session'
